74736aecb41d3fa1c7f45859353dfcaa44f98122
Jansen Rodrigo <169899722+jansencir@users.noreply.github.com>
2025-02-27 00:39:11 -1000

    Create method to append value to Linked List class

--- main.js
+++ main.js
@@ -12,4 +12,32 @@ class LinkedList {
     this.head = null;
     this.size = 0;
   }
+
+  // Add a new node, containing a value, to the end of the list
+  append(value) {
+    // Create a new node with class Node, and pass in the value
+    let node = new Node(value);
+    // Create a variable to hold the current node in the linked list
+    let current;
+
+    // If the linked list is empty, make the head the node
+    if (!this.head) {
+      this.head = node;
+    } else {
+      // If it's not empty, traverse the list until you hit the end
+      // Set current to the head of the liked list
+      current = this.head;
+
+      // While the current node has a next node, change current to it's next node
+      while(current.nextNode) {
+        current = current.nextNode;
+      }
+
+      // Once you've hit the end of the linked list, set the next node to node
+      current.nextNode = node;
+    }
+    this.size++;
+  }
+
+  
 }
