4e012997565250c11a3a7cda852382b62b2402f5
Jansen Rodrigo <169899722+jansencir@users.noreply.github.com>
2025-02-27 00:25:55 -1000

    Create Node class

---
+++ main.js
@@ -0,0 +1,7 @@
+// Create a Node class that holds a value property and a nextNode property
+class Node {
+  constructor(value = null, nextNode = null) {
+    this.value = value;
+    this.nextNode = nextNode;
+  }
+};
